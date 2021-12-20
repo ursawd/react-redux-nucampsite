02e95d6f6e627bd520bf257d6c9e89feced2a26d
Phil Bailey <philbailey@msn.com>
2021-12-19 23:18:45 -0500

    complete Exercise: React Router

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ import "bootstrap-social/bootstrap-social.css";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
+
   document.getElementById("root")
 );
