afed8fab2907c54abcc2da34f4deec74dcbb0271
Phil Bailey <philbailey@msn.com>
2021-12-21 15:07:23 -0500

    Exercise: React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import { CAMPSITES } from "../shared/campsites";
 import { COMMENTS } from "../shared/comments";
 import { PARTNERS } from "../shared/partners";
 import { PROMOTIONS } from "../shared/promotions";
+import CampsiteInfo from "./CampsiteInfoComponent";
 
 class Main extends Component {
   constructor(props) {
@@ -32,12 +33,22 @@ class Main extends Component {
       );
     };
 
+    const CampsiteWithId = ({ match }) => {
+      return (
+        <CampsiteInfo
+          campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+          comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/directory" render={() => <Directory campsites={this.state.campsites} />} />
+          <Route path="/directory/:campsiteId" component={CampsiteWithId} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
