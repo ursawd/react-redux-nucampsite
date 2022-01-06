986a836759ec23eb6fa10de4f7c7ee1cc63c0d70
Phil Bailey <philbailey@msn.com>
2022-01-06 10:44:38 -0500

    add redux actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -21,9 +21,8 @@ class CommentForm extends Component {
   }
 
   handleSubmit(values) {
-    console.log("Current modal values are: " + JSON.stringify(values));
-    alert("Current modal values are: " + JSON.stringify(values));
     this.toggleModal();
+    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
   }
 
   toggleModal() {
@@ -36,7 +35,7 @@ class CommentForm extends Component {
     return (
       <div>
         <Button outline onClick={this.toggleModal}>
-          <i class="fa fa-pencil fa-lg"></i> Submit Comment
+          <i className="fa fa-pencil fa-lg"></i> Submit Comment
         </Button>
         <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
           <ModalBody>
@@ -125,7 +124,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -143,7 +142,7 @@ function RenderComments({ comments }) {
             </div>
           );
         })}
-        <CommentForm />
+        <CommentForm campsiteId={campsiteId} addComment={addComment} />
       </div>
     );
   }
@@ -168,7 +167,7 @@ function CampsiteInfo(props) {
         </div>
         <div className="row">
           <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.comments} />
+          <RenderComments comments={props.comments} addComment={props.addComment} campsiteId={props.campsite.id} />
         </div>
       </div>
     );
